79e0701b977ef70d30b4d0e6de4ffb0b13241f1d
Alessandro Astarita <aleast@caprionline.it>
2016-11-16 14:59:59 +0100

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: [
-        "./src/SalsaCalendar.js",
-        //"./src/css/salsa-calendar.less"
+        "./src/SalsaCalendar.js"
     ],
     output: {
         path: __dirname,
